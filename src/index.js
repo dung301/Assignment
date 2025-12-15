ca080086a17cfec8e5fef73af2e6676037a7accf
Trần Anh Dũng <tanhdung301@gmail.com>
2025-12-15 23:53:18 +0700

    Resolve conflicts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom'
+<<<<<<< Updated upstream
 import CustomerScreen from './Customer/CustomerScreen';
 import Admin_dashboard from './Admin/Admin_dashboard';
 import PreOrderPage from './Customer/PreOrderInfor';
+=======
+>>>>>>> Stashed changes
 import './index.css'
-import Admin_dashboard from './Admin/AdminLayout';
 import App from './App';
-import Staff from './Staff/Staff';
-import TestRouter from './TestRouter';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
